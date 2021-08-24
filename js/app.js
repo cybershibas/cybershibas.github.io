bdec91b0b7976e28afbaa55f50941d07956e2c37
cybershibas <cybershibas@gmail.com>
2021-08-24 15:16:39 +0300

    up

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     $('.js-nav').removeClass('active');
     $('body').removeClass('overflow');
     $('html, body').animate({
-      scrollTop: $(href).offset().top
+      scrollTop: $(href).offset().top - 40
     }, 1000);
     return false;
   }); //burger
@@ -50,4 +50,65 @@ document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
 window.addEventListener('resize', function () {
   var vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
+});
+var arrOfData = [{
+  counter: 1,
+  setData: [{
+    srcImg: "images/h1-1.jpg",
+    textBtn: "SHIBA C41-V"
+  }, {
+    srcImg: "images/h1-2.jpg",
+    textBtn: "SHIBA C37-V"
+  }, {
+    srcImg: "images/h1-3.jpg",
+    textBtn: "SHIBA C43-V"
+  }]
+}, {
+  counter: 1,
+  setData: [{
+    srcImg: "images/h2-1.jpg",
+    textBtn: "SHIBA C45-V"
+  }, {
+    srcImg: "images/h2-2.jpg",
+    textBtn: "SHIBA C44-V"
+  }, {
+    srcImg: "images/h2-3.jpg",
+    textBtn: "SHIBA C38-V"
+  }]
+}, {
+  counter: 1,
+  setData: [{
+    srcImg: "images/h3-1.jpg",
+    textBtn: "SHIBA C39-V"
+  }, {
+    srcImg: "images/h3-2.jpg",
+    textBtn: "SHIBA C42-V"
+  }, {
+    srcImg: "images/h3-3.jpg",
+    textBtn: "SHIBA C36-V"
+  }]
+}];
+var cardWrap = document.querySelector(".card-wrap");
+cardWrap.addEventListener("click", function (event) {
+  if (!event.target.closest(".card__img-wrap") && !event.target.closest(".card__link")) {
+    return;
+  }
+
+  var indexCard = Array.from(cardWrap.children).findIndex(function (item) {
+    return item === event.target.closest(".card");
+  });
+  var neededData = arrOfData[indexCard];
+
+  if (neededData.counter === neededData.setData.length) {
+    neededData.counter = 0;
+  }
+
+  var targetCard = Array.from(cardWrap.children).find(function (item) {
+    return item === event.target.closest(".card");
+  });
+  var cardImg = targetCard.querySelector("img");
+  var cardBtn = targetCard.querySelector("button");
+  cardImg.src = neededData.setData[neededData.counter].srcImg;
+  cardBtn.textContent = neededData.setData[neededData.counter].textBtn;
+  neededData.counter += 1;
 });
